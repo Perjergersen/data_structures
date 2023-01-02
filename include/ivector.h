65ee98586005c5a354c88094fccda112c9a544d9
nathan <ntpedersen1@gmail.com>
2023-01-02 10:56:42 -0900

    format

--- include/ivector.h
+++ include/ivector.h
@@ -9,9 +9,9 @@
 typedef struct IVector IVector;
 
 IVector* ivector_create();
-void ivector_insert(IVector* iv, int data);
-void ivector_print(IVector* iv);
-int ivector_get(IVector* iv, int index);
-int ivector_size(IVector* iv);
+void     ivector_insert(IVector* iv, int data);
+void     ivector_print(IVector* iv);
+int      ivector_get(IVector* iv, int index);
+int      ivector_size(IVector* iv);
 
 #endif
--- src/ivector.c
+++ src/ivector.c
@@ -2,29 +2,32 @@
 
 struct IVector {
     int* vec;
-    int size;
-    int mem_alloc;
+    int  size;
+    int  mem_alloc;
 };
 
-IVector* ivector_create() {
-    IVector* iv = smalloc(sizeof(IVector));
+IVector*
+ivector_create() {
+    IVector* iv   = smalloc(sizeof(IVector));
     iv->mem_alloc = 2;
-    iv->vec = scalloc(iv->mem_alloc, sizeof(int));
-    iv->size = 0;
+    iv->vec       = scalloc(iv->mem_alloc, sizeof(int));
+    iv->size      = 0;
     return iv;
 }
 
-void ivector_insert(IVector* iv, int data) {
+void
+ivector_insert(IVector* iv, int data) {
     if (iv->size == iv->mem_alloc) {
         iv->mem_alloc *= 2;
         iv->vec = srealloc(iv->vec, iv->mem_alloc * sizeof(int));
-        //printf("new allocd size: %d\n", iv->mem_alloc);
+        // printf("new allocd size: %d\n", iv->mem_alloc);
     }
     iv->vec[iv->size] = data;
     iv->size++;
 }
 
-void ivector_print(IVector* iv) {
+void
+ivector_print(IVector* iv) {
     printf("[ ");
     for (int i = 0; i < iv->size; i++) {
         printf("%d ", iv->vec[i]);
@@ -32,17 +35,19 @@ void ivector_print(IVector* iv) {
     printf("]\n");
 }
 
-int ivector_get(IVector* iv, int index) {
+int
+ivector_get(IVector* iv, int index) {
     if (index < 0 || index >= iv->size) {
         fprintf(stderr, "ivector_get index out of bounds\n");
-        fprintf(stderr, "attempting to access index %d in size %d array\n", index, iv->size);
+        fprintf(stderr, "attempting to access index %d in size %d array\n",
+                index, iv->size);
         exit(EXIT_FAILURE);
     }
 
     return iv->vec[index];
 }
 
-int ivector_size(IVector* iv) {
+int
+ivector_size(IVector* iv) {
     return iv->size;
 }
-
