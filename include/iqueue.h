0e9af94fc9b963fc88c14a119b714d07ec9de8b1
perjergersen <ntpedersen@alaska.edu>
2023-01-04 06:02:48 -0900

    basic queue implementation

--- include/iqueue.h
+++ include/iqueue.h
@@ -1,13 +1,19 @@
 #ifndef IQUEUE
 #define IQUEUE
 
+#include <stdio.h>
+
+#include "safe_xallocs.h"
+
 typedef struct IQueueNode IQueueNode;
 typedef struct IQueue IQueue;
 
+IQueueNode* iqueuenode_create(int data);
 IQueue* iqueue_create();
 void iqueue_insert(IQueue* iq, int data);
-IQueueNode* iqueue_pop(IQueue* iq);
+int iqueue_pop(IQueue* iq);
 IQueueNode* iqueue_next(IQueue* iq);
+int iqueuenode_get_data(IQueueNode* iq);
 void iqueue_print(IQueue* iq);
 void iqueue_destroy(IQueue *iq);
 
--- src/iqueue.c
+++ src/iqueue.c
@@ -6,13 +6,82 @@ struct IQueueNode {
 };
 
 struct IQueue {
-    IQueueNode* first;
+    IQueueNode* head;
+    IQueueNode* tail;
+    int size;
 };
 
-IQueue* iqueue_create();
-void iqueue_insert(IQueue* iq, int data);
-IQueueNode* iqueue_pop(IQueue* iq);
-IQueueNode* iqueue_next(IQueue* iq);
-void iqueue_print(IQueue* iq);
-void iqueue_destroy(IQueue *iq);
+IQueueNode* iqueuenode_create(int data) {
+    IQueueNode* iqn = smalloc(sizeof(IQueueNode));
+    iqn->data = data;
+    iqn->next = NULL;
+    return iqn;
+}
+
+IQueue* iqueue_create() {
+    IQueue* iq = smalloc(sizeof(IQueue));
+    iq->head = NULL;
+    iq->tail = NULL;
+    iq->size = 0;
+    return iq;
+}
+
+void iqueue_insert(IQueue* iq, int data) {
+    IQueueNode* new_node = iqueuenode_create(data);
+    
+    if (iq->head == NULL) {
+        iq->head = new_node;
+        iq->tail = new_node;
+        iq->size += 1;
+        return;
+    }
+
+    iq->tail->next = new_node;
+    iq->tail = new_node;
+    iq->size += 1;
+}
+
+int iqueue_pop(IQueue* iq) {
+    if (iq == NULL || iq->size <= 0) {
+        fprintf(stderr, "iqueue_pop: IQueue* iq is either NULL or has size 0\n");
+        exit(EXIT_FAILURE);
+    }
+    int ret = iq->head->data;
+    IQueueNode* del_node = iq->head;
+    iq->head = iq->head->next;
+    free(del_node);
+    del_node = NULL;
+
+    iq->size -= 1;
+
+    return ret;
+}
+
+IQueueNode* iqueue_next(IQueue* iq) {
+    return iq->head;
+}
+void iqueue_print(IQueue* iq) {
+    IQueueNode* curr = iq->head;
+    while (curr) {
+        printf("%d ", curr->data);
+        curr = curr->next;
+    }
+    printf("\n");
+}
+
+int iqueuenode_get_data(IQueueNode* iq) {
+    return iq->data;
+}
+
+void iqueue_destroy(IQueue *iq) {
+    IQueueNode* curr = iq->head;
+    while (curr) {
+        IQueueNode* prev = curr;
+        curr = curr->next;
+        free(prev);
+        prev = NULL;
+    }
+    free(iq);
+    iq = NULL;
+}
 
